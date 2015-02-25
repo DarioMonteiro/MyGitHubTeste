ace56fc84afa28ecba5b0fc88b75b17c275de240
DarioMonteiro <dario@gir.srv.br>
2015-02-25 12:13:53 -0300

    meu teste de conflito

--- Program.cs
+++ Program.cs
@@ -10,11 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            
-            
-            
-            System.Console.WriteLine("dario@gir.srv.br");
-          
+            System.Console.Write("Oi "); 
+            System.Console.WriteLine("dario@gir.srv.br");          
         }
     }
 }
